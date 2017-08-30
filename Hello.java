7d14891e8401fb404c8f20978cd8b9292b598c95
MOTIF\chadaporn.s <chadaporn.s@motiftech.com>
2017-08-30 19:21:45 +0700

    no message

---
+++ Hello.java
@@ -0,0 +1,18 @@
+
+
+class Hello {
+    
+    public Hello() {
+        System.out.println("Hello! We are:");
+        this.Krerk();
+        this.Panjapoll();
+    }
+    
+    private void Krerk() {
+       System.out.println("  Trainee - Chadaporn Sukcham, ASA");
+    }
+
+      public static void main(String [] argv) {
+        new Hello();
+    }
+}
